3b72ff43d9d4d8692a849c34a5597b6bfb4a33ce
Syed Roshaan ur Rehman <roshaan.rehman@gmail.com>
2023-04-10 03:15:50 +0500

    Enhancement reporting (#2)
    * Removed unused files and packages
    
    * Removed unused files
    
    * Added reporting
    
    ---------

--- RestSharpSpecflowFramework/Hooks/Hooks.cs
+++ RestSharpSpecflowFramework/Hooks/Hooks.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using Microsoft.Extensions.Configuration;
 using RestSharpSpecflowFramework.Configuration;
 using RestSharpSpecflowFramework.Drivers;
+using RestSharpSpecflowFramework.Reporting;
 using RestSharpSpecflowFramework.Support;
 using TechTalk.SpecFlow;
 
 namespace RestSharpSpecflowFramework.Hooks
 {
     [Binding]
-    public sealed class Hooks
+    public sealed class Hooks : ExtentReport
     {
         [BeforeTestRun]
         public static void Setup()
@@ -21,6 +24,91 @@ namespace RestSharpSpecflowFramework.Hooks
             APIHelper.baseURL = config.ApiBaseURL;
 
             RestSharpManager.InitializeEndpoint(APIHelper.baseURL);
+        }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            Console.WriteLine("Running before test run...");
+            ExtentReportInit();
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            Console.WriteLine("Running after test run...");
+            ExtentReportTearDown();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            Console.WriteLine("Running before feature...");
+            _feature = _extentReports.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+        [BeforeScenario(Order = 1)]
+        public void FirstBeforeScenario(ScenarioContext scenarioContext)
+        {
+            Console.WriteLine("Running before scenario...");
+            _scenario = _feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
+        }
+
+        [AfterStep]
+        public void AfterStep(ScenarioContext scenarioContext)
+        {
+            Console.WriteLine("Running after step....");
+            string stepType = scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+            string stepName = scenarioContext.StepContext.StepInfo.Text;
+
+            //When scenario passed
+            if (scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName);
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName);
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName);
+                }
+                else if (stepType == "And")
+                {
+                    _scenario.CreateNode<And>(stepName);
+                }
+            }
+
+            //When scenario fails
+            if (scenarioContext.TestError != null)
+            {
+
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName).Fail(scenarioContext.TestError.Message);
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName).Fail(scenarioContext.TestError.Message);
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName).Fail(scenarioContext.TestError.Message);
+                }
+                else if (stepType == "And")
+                {
+                    _scenario.CreateNode<And>(stepName).Fail(scenarioContext.TestError.Message);
+                }
+            }
+        }
+
+        [AfterFeature]
+        public static void AfterFeature()
+        {
+            Console.WriteLine("Running after feature...");
         }
 
         [AfterScenario]
---
+++ RestSharpSpecflowFramework/Reporting/ExtentReport.cs
@@ -0,0 +1,39 @@
+﻿using AventStack.ExtentReports.Reporter.Configuration;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpSpecflowFramework.Reporting
+{
+    public class ExtentReport
+    {
+        public static ExtentReports _extentReports;
+        public static ExtentTest _feature;
+        public static ExtentTest _scenario;
+
+        public static String dir = AppDomain.CurrentDomain.BaseDirectory;
+        public static String testResultPath = dir.Replace("bin\\Debug\\net6.0", "TestResults");
+
+        public static void ExtentReportInit()
+        {
+            var htmlReporter = new ExtentHtmlReporter(testResultPath);
+            htmlReporter.Config.ReportName = "Automation Status Report";
+            htmlReporter.Config.DocumentTitle = "Automation Status Report";
+            htmlReporter.Config.Theme = Theme.Standard;
+            htmlReporter.Start();
+
+            _extentReports = new ExtentReports();
+            _extentReports.AttachReporter(htmlReporter);
+            _extentReports.AddSystemInfo("Browser", "Chrome");
+        }
+
+        public static void ExtentReportTearDown()
+        {
+            _extentReports.Flush();
+        }
+    }
+}
